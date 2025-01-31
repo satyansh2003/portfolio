9a8c45fbd39889bde596c3b967647057d304bc6a
satyansh2003 <123368066+satyansh2003@users.noreply.github.com>
2025-01-31 06:14:50 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,10 +9,10 @@ const Hero = () => (
     <LeftSection>
       <SectionTitle main center>
         Welcome To <br/>
-        My Personal Portfolio
+        My Portfolio
       </SectionTitle>
       <SectionText>
-      Passionate about crafting innovative digital experiences, I am a Computer Science and Engineering student with a keen interest in web development, software engineering, and problem-solving. From building modern web applications to working on interdisciplinary projects, I thrive on creating impactful solutions. Explore my portfolio to discover my journey, projects, and skills that reflect my commitment to technology and continuous learning.
+      Passionate about crafting innovative digital experiences, I am SATYANSH KUMAR, a Computer Science and Engineering student interested in web development, software engineering, and problem-solving. From building modern web applications to working on interdisciplinary projects, I thrive on creating impactful solutions. Explore my portfolio to discover my journey, projects, and skills that reflect my commitment to technology and continuous learning.
       </SectionText>
       <Button onClick={()=> window.location = 'https://drive.google.com/file/d/1CK3_C3XKTAP3h85eZOyjVMCpnG92tVqn/view?usp=sharing'}>Learn More</Button>
     </LeftSection>
